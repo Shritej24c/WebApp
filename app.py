e90ff3fcd4bdb47e1eae11ac008a7e1969d8cdd0
Shritej24c <46397283+Shritej24c@users.noreply.github.com>
2019-05-26 09:45:34 +0530

    Keras with Flask

--- app.py
+++ app.py
@@ -1,8 +1,58 @@
 from flask import Flask, jsonify, request, render_template
-
+from PIL import Image
+import base64
+from keras.models import load_model
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, img_to_array
+import io
+import tensorflow as tf
+from keras import backend as K
 import os
 
 app = Flask(__name__)
+global graph
+
+def get_model():
+    global model
+    model = load_model('cifar10_classifier.h5')
+    print("Model Loaded")
+
+
+def preprocessing_image(image, target_size):
+    if image.mode != 'RGB':
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+
+print("Loading the keras model")
+get_model()
+graph = tf.get_default_graph()
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocessing_image(image, target_size=(32,32))
+    with graph.as_default():
+        prediction = model.predict_classes(processed_image)
+        response = {
+            'yclass': int(prediction[0])
+    }
+
+    return jsonify(response)
+
+
+'''
+@app.route('/')
+def index():
+    return render_template("page.html")
 
 
 @app.route('/hello', methods=['POST'])
@@ -11,14 +61,19 @@ def hello():
     name = message['name']
     response = {
         'greeting': 'Hello,' + name + '!'
-
     }
     return jsonify(response)
 
 
+
+@app.route('/')
+def page():
+    return "Good Morning Mumbai"
+'''
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(port=port, debug=True)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
 
 
 '''
